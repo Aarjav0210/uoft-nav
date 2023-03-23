660b09bc6a40186a1005b65b0f5fee936d971b73
Aarjav0210 <Aarjav02@gmail.com>
2023-03-23 14:58:36 -0400

    Cleanse main.py

--- main.py
+++ main.py
@@ -10,23 +10,35 @@ pic_base = 'https://maps.googleapis.com/maps/api/streetview?'
 
 location = '27 King\'s College Cir, Toronto, ON M5S'
 
-meta_params = {'key': api_key, 'location': location}
-pic_params = {'key': api_key, 'location': location, 'size': '640x640'}
+# define the params for the metadata reques
+meta_params = {'key': api_key,
+               'location': location
+               }
+# define the params for the picture request
+pic_params = {'key': api_key,
+              'location': location,
+              'size': '640x640'
+              }
 
+# obtain the metadata of the request (this is free)
 meta_response = requests.get(meta_base, params=meta_params)
-#print(meta_response.json())
+print(meta_response.json())
 
 pic_response = requests.get(pic_base, params=pic_params)
 
 for key, value in pic_response.headers.items():
     print(f"{key}: {value}")
 
-with open('test.jpg', 'wb') as file:
+print(pic_response.ok)
+
+filename = './images/test.jpg' 
+
+with open(filename, 'wb') as file:
     file.write(pic_response.content)
 # remember to close the response connection to the API
 pic_response.close()
 
 plt.figure(figsize=(10, 10))
-img=mpimg.imread('test.jpg')
+img=mpimg.imread(filename)
 imgplot = plt.imshow(img)
 plt.show()
