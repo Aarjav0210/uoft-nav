fbd5dce821fef217aa6dd3276cba885e03d10efb
Aarjav0210 <Aarjav02@gmail.com>
2023-04-11 18:53:49 -0400

    Create classes for cleaner code

---
+++ csv_reader.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from streetview import StreetViewer
+
+
+#read csv file
+class CSVReader:
+    def __init__(self, filename):
+        self.filename = filename
+        self.filepath = os.path.join(os.getcwd(), filename)
+        self.df = pd.read_csv(self.filepath)
+
+    #return list of all tags from the 'tag' column
+    def get_all_tags(self):
+        #dont add any tags that have a 'N/A' in for LatLong
+        df = self.df
+        df = df[df['LatLong'] != 'N/A']
+        tags = df['Tag'].tolist()
+        return tags
+
+    def get_row(self, tag):
+        #get row from csv file
+        df = self.df
+        row = df.loc[df['Tag'] == tag]
+        #return row as a dict
+        return row.to_dict('records')[0]
+
+    def print_row(row):
+        print(row)
+
+    def get_image(self, tag):
+        row = self.get_row(tag)
+        location = row['LatLong']
+        fov = row['FOV']
+        heading = row['Heading']
+        pitch = row['Pitch']
+        image_viewer = StreetViewer(api_key=os.getenv('API_KEY'), location=location, fov=fov, heading=heading, pitch=pitch, verbose=True)
+        image_viewer.get_meta()
+        image_viewer.get_pic()
+        image_viewer.display_pic()
--- main.py
+++ main.py
@@ -6,90 +6,17 @@ import matplotlib.image as mpimg
 from streetview import StreetViewer
 #for csv file
 import pandas as pd
-#for API key
 import os
 from dotenv import load_dotenv
+from csv_reader import CSVReader
 
 #load API key
 load_dotenv()
 
+#load data from csv file
+filename = 'uoft_locations.csv'
+uoft_data = CSVReader(filename)
 
-#read csv file
-def read_csv(filename):
-    path_to_file = os.path.join(os.getcwd(), filename)
-    df = pd.read_csv(path_to_file)
-    return df
+#Get image
+uoft_data.get_image(uoft_data.get_all_tags()[0])
 
-#return list of all tags from the 'tag' column
-def get_all_tags(df):
-    #dont add any tags that have a 'N/A' in for LatLong
-    df = df[df['LatLong'] != 'N/A']
-    tags = df['Tag'].tolist()
-    return tags
-
-def get_row(df, tag):
-    #get row from csv file
-    row = df.loc[df['Tag'] == tag]
-    #return row as a dict
-    return row.to_dict('records')[0]
-
-def print_row(row):
-    print(row)
-
-uoft_locations_df = read_csv('uoft_locations.csv')
-# print_row(get_row(uoft_locations_df, get_all_tags(uoft_locations_df)[0]))
-# print_row(get_row(uoft_locations_df, get_all_tags(uoft_locations_df)[1]))
-
-
-def get_image(tag):
-    row = get_row(uoft_locations_df, tag)
-    location = row['LatLong']
-    fov = row['FOV']
-    heading = row['Heading']
-    pitch = row['Pitch']
-    image_viewer = StreetViewer(api_key=os.getenv('API_KEY'), location=location, fov=fov, heading=heading, pitch=pitch, verbose=True)
-    image_viewer.get_meta()
-    image_viewer.get_pic()
-    image_viewer.display_pic()
-
-get_image(get_all_tags(uoft_locations_df)[0])
-
-# image_viewer = StreetViewer(api_key=os.getenv('API_KEY'), location='88 King\'s College Cir, Toronto, ON M5S', verbose=True)
-
-# meta_base = 'https://maps.googleapis.com/maps/api/streetview/metadata?'
-# pic_base = 'https://maps.googleapis.com/maps/api/streetview?'
-
-# location = '88 King\'s College Cir, Toronto, ON M5S'
-
-# # define the params for the metadata request
-# meta_params = {'key': os.getenv('API_KEY'),
-#                'location': location
-#                }
-# # define the params for the picture request
-# pic_params = {'key': os.getenv('API_KEY'),
-#               'location': location,
-#               'size': '640x640'
-#               }
-
-# # obtain the metadata of the request (this is free)
-# meta_response = requests.get(meta_base, params=meta_params)
-# print(meta_response.json())
-
-# pic_response = requests.get(pic_base, params=pic_params)
-
-# for key, value in pic_response.headers.items():
-#     print(f"{key}: {value}")
-
-# print(pic_response.ok)
-
-# filename = './images/test.jpg' 
-
-# with open(filename, 'wb') as file:
-#     file.write(pic_response.content)
-# # remember to close the response connection to the API
-# pic_response.close()
-
-# plt.figure(figsize=(10, 10))
-# img=mpimg.imread(filename)
-# imgplot = plt.imshow(img)
-# plt.show()
