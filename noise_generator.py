e7718402092c837a08361b54d8a1b7334fb2fbda
Aarjav0210 <Aarjav02@gmail.com>
2023-04-18 15:36:11 -0400

    Update dataLoader to save image batch (with noise)

--- noise_generator.py
+++ noise_generator.py
@@ -13,8 +13,30 @@ class NoiseGenerator(object):
         img = cv2.imread(img_path)
         return img
 
-    def generate_noise(self, img_path, noise_type):
-        img = self._get_image(img_path)
+    # def generate_noise(self, img_path, noise_type):
+    #     img = self._get_image(img_path)
+    #     if noise_type == 'low':
+    #         noise = self.noise_low
+    #     elif noise_type == 'high':
+    #         noise = self.noise_high
+    #     else:
+    #         raise ValueError('Noise type must be either "low" or "high"')
+    #     noisy_img = cv2.add(img, noise, dtype=cv2.CV_8U)
+    #     return noisy_img
+
+    # def generate_noise(self, image_content, noise_type):
+    #     img = cv2.imdecode(np.frombuffer(image_content, np.uint8), cv2.IMREAD_COLOR)
+    #     if noise_type == 'low':
+    #         noise = self.noise_low
+    #     elif noise_type == 'high':
+    #         noise = self.noise_high
+    #     else:
+    #         raise ValueError('Noise type must be either "low" or "high"')
+    #     noisy_img = cv2.add(img, noise, dtype=cv2.CV_8U)
+    #     return noisy_img.astype(np.uint8)
+
+    def generate_noise(self, image_content, noise_type):
+        img = cv2.imdecode(np.frombuffer(image_content, np.uint8), cv2.IMREAD_COLOR)
         if noise_type == 'low':
             noise = self.noise_low
         elif noise_type == 'high':
@@ -22,7 +44,15 @@ class NoiseGenerator(object):
         else:
             raise ValueError('Noise type must be either "low" or "high"')
         noisy_img = cv2.add(img, noise, dtype=cv2.CV_8U)
-        return noisy_img
+
+        # Encode the noisy image as a PNG file in memory
+        _, buffer = cv2.imencode('.png', noisy_img)
+
+        # Convert the image buffer to a binary string
+        image_content = buffer.tobytes()
+
+        return image_content
+
     
     def show_noise(self, img_path, noise_type):
         noisy_img = self.generate_noise(img_path, noise_type)
