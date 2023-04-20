739a8688fe5cce332edfb971ff1995937428ec7d
1cristeaste <stcristea@gmail.com>
2023-04-19 22:18:22 -0400

    plotting

--- plotting-val.py
+++ plotting-val.py
@@ -3,22 +3,35 @@ import matplotlib.pyplot as plt
 
 # Read data from the txt file
 with open('resnet18results.txt', 'r') as f:
-    content = f.read()
+    content_18 = f.read()
+
+with open('resnet34 loss.txt', 'r') as f:
+    content_34 = f.read()
+
+with open('VGG16 loss.txt', 'r') as f:
+    content_16 = f.read()
 
 # Extract train and validation loss values
-train_losses = re.findall(r'Train Loss: (\d+\.\d+)', content)
-val_losses = re.findall(r'Val Loss: (\d+\.\d+)', content)
+train_losses_18 = re.findall(r'Train Loss: (\d+\.\d+)', content_18)
+train_losses_34 = re.findall(r'\[\d+\] loss: (\d+\.\d+)', content_34)
+train_losses_16 = re.findall(r'\[\d+\] loss: (\d+\.\d+)', content_16)
 
 # Convert the loss values from strings to floats
-train_losses = [float(loss) for loss in train_losses]
-val_losses = [float(loss) for loss in val_losses]
+train_losses_18 = [float(loss) for loss in train_losses_18]
+train_losses_34 = [float(loss) for loss in train_losses_34]
+train_losses_16 = [float(loss) for loss in train_losses_16]
 
 # Plot the data
-epochs = range(1, len(train_losses) + 1)
-plt.plot(epochs, train_losses, 'r', label='Training loss')
-plt.plot(epochs, val_losses, 'b', label='Validation loss')
+# Create separate epochs lists for each ResNet model
+epochs_18 = range(1, len(train_losses_18) + 1)
+epochs_34 = range(1, len(train_losses_34) + 1)
+epochs_16 = range(1, len(train_losses_16) + 1)
 
-plt.title('ResNet18: Training and Validation Loss')
+# Plot the data
+plt.plot(epochs_18, train_losses_18, 'r', label='ResNet18')
+plt.plot(epochs_34, train_losses_34, 'b', label='ResNet34')
+plt.plot(epochs_16, train_losses_16, 'g', label='VGG16')
+plt.title('Training Loss Comparison')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
