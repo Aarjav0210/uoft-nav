61b71ea30a6e9ef1311d423187678f3fe74a9989
1cristeaste <stcristea@gmail.com>
2023-04-19 22:39:37 -0400

    cleaning code

--- VGG16.py
+++ VGG16.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms, models
+import splitfolders
 import matplotlib.pyplot as plt
 import torch.multiprocessing as mp
 
